998fa9e271777ccd7000c877d4e538c640fda6dd
luisFebro <mr.febro@gmail.com>
2022-08-28 09:48:59 -0300

    first

---
+++ app/src/main/java/com/febro/bottomnavfloatbtn/MainActivity.java
@@ -0,0 +1,15 @@
+// ref: https://www.youtube.com/watch?v=iEXh1-KVeVc
+package com.febro.bottomnavfloatbtn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
